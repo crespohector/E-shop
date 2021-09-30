6782fb47efd863c13964c5358c47efb8d2710407
Hector <crespomhector@gmail.com>
2021-09-30 14:10:22 -0400

    uploaded eshop logo

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
+import eShopImg from "../../images/eShopImg.png"
 import "./NavBar.css"
 
 const NavBar = () => {
+  const [search, setSearch] = useState('');
+
   return (
     <nav className="main_wrapper">
       <div className="wrapper">
-        <input 
-      </div>
-      <div className="wrapper">
 
+        <div className="wrapper-search">
+          <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" />
+          <i className="fas fa-search"></i>
+        </div>
+
+        <img src={eShopImg} alt="EShopImage" />
+
+        <div className="wrapper-user">
+          <div className="userBtn">
+            <i className="far fa-user"></i>
+          </div>
+          <div className="userShopCart">
+            <i class="fas fa-shopping-cart"></i>
+          </div>
+        </div>
+
+      </div>
+
+      <div className="wrapper">
+        <button type="button">Women</button>
+        <button type="button">Men</button>
+        <button type="button">Toys</button>
+        <button type="button">Technology</button>
       </div>
       {/* <ul>
         <li>
