90053177570f89ac492a3e38eee158147dd1d046
Hector <crespomhector@gmail.com>
2022-02-02 03:14:56 -0500

    added state to display for search box, applied more dynamic styling

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -7,24 +7,29 @@ import "./NavBar.css"
 
 const NavBar = () => {
   const [search, setSearch] = useState('');
+  const [display, setDisplay] = useState(false);
   const products = useSelector(state => state.products);
   const productsArr = Object.values(products);
 
-  console.log('products: ', productsArr)
-  console.log('search: ', search)
+  console.log('----display: ', display);
+
+  const onClickSearchBox = () => {
+    setDisplay(true);
+
+  }
 
   return (
     <nav className="navbar">
 
-      <div className="search-bar">
-        <input className="search-bar__input" type="search" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Search" />
+      <div className={!display ? "search-bar" : "search-bar--focus"}>
+        <input className="search-bar__input" type="search" value={search} onClick={onClickSearchBox} onChange={(e) => setSearch(e.target.value)} placeholder="Search" />
         <i className="fas fa-search"></i>
-        {search.length > 0 && <div className='search-bar__results-container'>
+        {(search.length > 0 && display === true) && <div className='search-bar__results-container' >
           {productsArr.filter(product => product.title.toLowerCase().includes(search)).map(product => (
             // {"Please Note that in the NavLink, if we remove the first slash '/' in the to attribute. What will happen is, if the
             // user keeps on clicking a the navlink in the results container, if will keep appending the same url. Thus, making it looks extremly buggy.
             // Solution: the first slash must be included. Becasue the navlink has to match the same route as specified."}
-            <NavLink key={product.id} className='search-filter-container__link' to={`/products/${product.id}`}>{product.title}</NavLink>
+            <NavLink key={product.id} className='search-bar__result-link' to={`/products/${product.id}`}>{product.title}</NavLink>
           ))}
         </div>}
       </div>
