02db4a442f75819263836a118aa2447908835828
Hector <crespomhector@gmail.com>
2022-02-27 23:51:17 -0500

    completed single page for product info

--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -3,11 +3,17 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchOneProduct } from "../../store/product";
 
+import "./ProductPage.css";
 
 const ProductPage = () => {
     const dispatch = useDispatch();
-    const {productId} = useParams();
+    const { productId } = useParams();
     const product = useSelector((state) => state.products[productId])
+
+    const AddtoCart = () => {
+        //add item to localstorage so it never gets delete while user refreshes page or leaves site.
+        //add it to shopping cart
+    }
 
     useEffect(() => {
         dispatch(fetchOneProduct(productId))
@@ -16,10 +22,11 @@ const ProductPage = () => {
     return (
         <div className="product-info">
             <img className="product-info__img" src={product?.image} alt={product?.title} />
-            <span className="product-info__title">{product?.title}</span>
-            <span className="product-info__category">{product?.title}</span>
-            <span className="product-info__description">{product?.title}</span>
-
+                <span className="product-info__title">{product?.title}</span>
+                <span className="product-info__category">Category: {product?.category}</span>
+                <span className="product-info__description">Description: {product?.description}</span>
+                <span className="product-info__price">${product?.price}</span>
+                <button onClick={AddtoCart} className="product-info__add-to-cart" type="button">Add to cart</button>
         </div>
     )
 }
