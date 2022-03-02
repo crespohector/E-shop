6866764c3abb4c27a80261fb4ba34f2c3314ac8f
Hector <crespomhector@gmail.com>
2022-03-01 23:22:35 -0500

    completed adding items to cart using local storage

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -3,20 +3,16 @@ import CartItem from "./CartItem";
 
 
 const ShoppingCart = () => {
-    //we need a list of all the current items from local storage.
-    //check if user is login
-    //after checking out, will create a new Order to the user.
     const [total, setTotal] = useState(0);
+    console.log('local storage: ', localStorage.getItem('items'))
 
     return (
-        <>
-            <div className="shopping-bag">
-                <p className="shopping-bag__title">Shopping Bag</p>
-                {/* <CartItem/> render each cart item from user */}
-                <span className="shopping-bag__total">{`Total $${total}`}</span>
-                <button type="button">Checkout</button>
-            </div>
-        </>
+        <div className="shopping-bag">
+            <p className="shopping-bag__title">Shopping Bag</p>
+            {/* <CartItem/> render each cart item from user */}
+            <span className="shopping-bag__total">{`Total $${total}`}</span>
+            <button type="button">Checkout</button>
+        </div>
     )
 }
 
--- react-app/src/components/ProductPage/index.js
+++ react-app/src/components/ProductPage/index.js
@@ -6,27 +6,38 @@ import { fetchOneProduct } from "../../store/product";
 import "./ProductPage.css";
 
 const ProductPage = () => {
+    const [error, setError] = useState("");
     const dispatch = useDispatch();
     const { productId } = useParams();
     const product = useSelector((state) => state.products[productId])
 
     const AddtoCart = () => {
-        //add item to localstorage so it never gets delete while user refreshes page or leaves site.
-        //add it to shopping cart
+        let items = localStorage.getItem('items');
+        if (items === null) {
+            localStorage.setItem('items', `${productId},`)
+            return ;
+        }
+        if (items.includes(productId)) {
+            setError("This item is already added to cart.")
+            return ;
+        }
+        items += `${productId}`
+        localStorage.setItem('items', items);
     }
 
     useEffect(() => {
         dispatch(fetchOneProduct(productId))
-    }, [])
+    }, [dispatch])
 
     return (
         <div className="product-info">
             <img className="product-info__img" src={product?.image} alt={product?.title} />
-                <span className="product-info__title">{product?.title}</span>
-                <span className="product-info__category">Category: {product?.category}</span>
-                <span className="product-info__description">Description: {product?.description}</span>
-                <span className="product-info__price">${product?.price}</span>
-                <button onClick={AddtoCart} className="product-info__add-to-cart" type="button">Add to cart</button>
+            <span className="product-info__title">{product?.title}</span>
+            <span className="product-info__category">Category: {product?.category}</span>
+            <span className="product-info__description">Description: {product?.description}</span>
+            <span className="product-info__price">${product?.price}</span>
+            {error.length > 0 ? <span className="product-info__error">{error}</span> : null}
+            <button onClick={AddtoCart} className="product-info__add-to-cart" type="button">Add to cart</button>
         </div>
     )
 }
